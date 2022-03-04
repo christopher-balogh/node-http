701266f533f3ec3f827c55ebd0802dd6f4203e8e
Christopher Balogh <chrisconcrete@gmail.com>
2022-03-04 08:41:20 -0700

    added server file

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const server = http.createServer((request, response) => {
+  response.end("Hello Galvanize!");
+});
+
+server.listen(3000, "127.0.0.1", () => {
+  console.log(`Server running at http://127.0.0.1:3000/`);
+});
